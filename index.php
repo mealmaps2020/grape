8ca07dfa86cda4c6d900b0fe9a314d8f531fecf3
mealmaps2020 <75931501+mealmaps2020@users.noreply.github.com>
2020-12-13 13:57:05 +0000

    Update index.php

--- index.php
+++ index.php
@@ -186,7 +186,7 @@
                           		<a href="#"><i class="fab fa-twitter"></i></a>
                           	 </li>
                           	 <li>
-                          		<a href="#"><i class="fab fa-facebook-square"></i></a>
+                          		<a href="https://www.instagram.com/meal_maps/"><i class="fab fa-instagram"></i></a>
                           	 </li>
                           	 <li>
                           		<a href="#"><i class="fab fa-pinterest"></i></a>
