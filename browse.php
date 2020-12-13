0a4f9edeb72b726a4079582499e4515e90b12dcd
mealmaps2020 <75931501+mealmaps2020@users.noreply.github.com>
2020-12-13 15:21:00 +0000

    Update browse.php

--- browse.php
+++ browse.php
@@ -36,7 +36,8 @@
                 </ul>
 		    </nav>
 	    </div>
-		
+		<div>
+			
 		<br>
 		<br>
 		<br>
@@ -64,7 +65,7 @@
 			</h2>
 		</h1>
 	
-  <section class="container3" id="list2">
+  <section class="containerhero" id="list2">
        <div class="item">
         <img src="https://lh3.googleusercontent.com/proxy/YBlZhGF7Qcsu1Omx-o9a29RWnU3GrX9_Qa9__O3Qzqy9LUmNEnj_jSOqjVZcybodMiY6nHWV7vbik7TgUf3pdlURhQ8fv10hkEy-xOt65NJytx7XYA" height="50">
         <h2>Flannagans Bar and Restaurant</h2>
