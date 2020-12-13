7d67c5849778e541531136ec5f78f278656f8a67
mealmaps2020 <75931501+mealmaps2020@users.noreply.github.com>
2020-12-13 17:59:42 +0000

    Update register.php

--- register.php
+++ register.php
@@ -1,52 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8"/>
-    <title>Registration</title>
-    <link rel="stylesheet" href="style.css"/>
+  <title>Meal Map</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<?php
-    require('db.php');
-    // When form submitted, insert values into the database.
-    if (isset($_REQUEST['username'])) {
-        // removes backslashes
-        $username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
-        $username = mysqli_real_escape_string($con, $username);
-        $email    = stripslashes($_REQUEST['email']);
-        $email    = mysqli_real_escape_string($con, $email);
-        $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
-        $create_datetime = date("Y-m-d H:i:s");
-        $query    = "INSERT into `users` (username, password, email, create_datetime)
-                     VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
-        $result   = mysqli_query($con, $query);
-        if ($result) {
-            echo "<div class='form'>
-                  <h3>You are registered successfully.</h3><br/>
-                  <p class='link'>Click here to <a href='login.php'>Login</a></p>
-                  </div>";
-        } else {
-            echo "<div class='form'>
-                  <h3>Required fields are missing.</h3><br/>
-                  <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
-                  </div>";
-        }
-    } else {
-?>
-    <form class="form" action="" method="post">
-        <h1 class="login-title">Registration</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Adress">
-        <input type="password" class="login-input" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link"><a href="login.php">Click to Login</a></p>
-    </form>
-<?php
-    }
-?>
+  <div class="header">
+    <h2>Register</h2>
+  </div>
+  
+  <form method="post" action="register.php">
+    <?php include('errors.php'); ?>
+    <div class="input-group">
+      <label>Username</label>
+      <input type="text" name="username" value="<?php echo $username; ?>">
+    </div>
+    <div class="input-group">
+      <label>Email</label>
+      <input type="email" name="email" value="<?php echo $email; ?>">
+    </div>
+    <div class="input-group">
+      <label>Password</label>
+      <input type="password" name="password_1">
+    </div>
+    <div class="input-group">
+      <label>Confirm password</label>
+      <input type="password" name="password_2">
+    </div>
+    <div class="input-group">
+      <button type="submit" class="btn" name="reg_user">Register</button>
+    </div>
+    <p>
+      Already a member? <a href="login.php">Sign in</a>
+    </p>
+  </form>
 </body>
 </html>
 
-	
